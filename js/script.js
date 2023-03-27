5346abcdf31bbdeb3d8951ad69ad8ac752e97bc2
anhdang00 <122416016+anhdang00@users.noreply.github.com>
2023-03-27 13:45:39 -0500

    adding event listener to button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+  var content = document.getElementById('entryinput');
+  var button = document.getElementById('entrybutton');
+  button.addEventListener('click', function(){
+  	alert("Anh Dang: " + content.value);
+  });
+ };
 
 
 
